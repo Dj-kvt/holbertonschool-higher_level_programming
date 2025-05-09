5c92f3dfcaa456c5262a1715ad1283573383c4d0
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-05-09 16:55:03 +0200

    add all file correct for the task of project module and import

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+arg_count = len(sys.argv) - 1
+
+if arg_count == 0:
+    print("0 arguments.")
+elif arg_count == 1:
+    print("1 argument:")
+else:
+    print(f"{arg_count} arguments:")
+
+for i in range(1, len(sys.argv)):
+    print(f"{i}: {sys.argv[i]}")
---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import importlib.util
+
+if __name__ == "__main__":
+    # Chemin vers le module compilé hidden_4.pyc présent dans /tmp
+    pyc_path = "/tmp/hidden_4.pyc"
+
+    # Charger le module depuis le fichier .pyc
+    spec = importlib.util.spec_from_file_location("hidden_4", pyc_path)
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
+
+    # Récupérer toutes les clés (noms) dans le dictionnaire du module
+    names = hidden_4.__dict__.keys()
+
+    # Filtrer les noms pour ne garder que ceux qui ne commencent pas par "__" et les trier par ordre alphabétique
+    for name in sorted(n for n in names if not n.startswith("__")):
+        print(name)
