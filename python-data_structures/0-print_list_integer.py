5342615c9bcd938fd7d5db85b1bee30f390b96ad
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-05-13 10:13:35 +0200

    change my cofe for the task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,7 @@
 def print_list_integer(my_list=[]):
     """Print all integers of a list."""
     for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]), end="\n" if i == len(my_list) - 1 else "\n")
+        if i != len(my_list) - 1:
+            print("{:d}".format(my_list[i]), end="\n")
+        else:
+            print("{:d}".format(my_list[i]), end="")
