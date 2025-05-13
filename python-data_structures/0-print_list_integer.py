8926c7ccec5923418b381fd9d49762091797b6ad
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-05-13 10:15:40 +0200

    add line return

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -5,4 +5,4 @@ def print_list_integer(my_list=[]):
         if i != len(my_list) - 1:
             print("{:d}".format(my_list[i]), end="\n")
         else:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="\n")
