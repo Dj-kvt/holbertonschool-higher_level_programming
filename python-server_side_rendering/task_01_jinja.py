90a5d2428ddffa606ec0d3cfdc847507dcca2b64
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-07-18 22:23:48 +0200

    make the takss two of project

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Flask application that renders templates with reusable components"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if name == '__main__':
+    app.run(debug=True, port=5000)
